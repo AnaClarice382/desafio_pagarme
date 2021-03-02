481cad4ff417d7546d2966b19052aff90fd10f37
AnaClarice382 <anaclaricep@gmail.com>
2021-03-02 18:33:33 -0300

    init method

--- __main__.py
+++ __main__.py
@@ -1,4 +1,7 @@
-a = db()
-a.create_tables()
-a.load_csv()
-a.pre_insert_companies()
+def __init__():
+    a = db()
+    a.create_tables()
+    a.load_csv()
+    a.pre_insert_companies()
+
+__init__()
